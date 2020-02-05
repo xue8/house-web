9f50ac1914d3f0671ed940356b50ca909e3dbf19
xue8 <rongyuexue@gmail.com>
2020-02-05 11:28:45 +0800

    frist commit

---
+++ vue.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        // ws : true,
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': ''
+        }
+      }
+    },
+    port: 80,
+    disableHostCheck: true,
+  },
+  configureWebpack: {
+    externals: {
+      "BMap": "BMap"
+    }
+  }
+}
